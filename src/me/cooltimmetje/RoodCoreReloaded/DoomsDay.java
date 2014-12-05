346ffeeca5ff0bb013f150d30828493310b0e376
cooltimmetje <cooltimmetje@me.com>
2014-12-05 21:45:13 +0100

    Doomsday command improved

--- src/me/cooltimmetje/RoodCoreReloaded/DoomsDay.java
+++ src/me/cooltimmetje/RoodCoreReloaded/DoomsDay.java
@@ -3,8 +3,10 @@ package me.cooltimmetje.RoodCoreReloaded;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Player;
 
+@SuppressWarnings("deprecation")
 public class DoomsDay implements CommandExecutor {
 
 
@@ -12,7 +14,21 @@ public class DoomsDay implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player p = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("doomsday")) {
-			p.sendMessage(Main.NoTag + "BOOM! i guess... :s");
+			if (p.getName().equals("ThoThoKill")){
+				p.sendMessage(Main.NoTag + "PAYBACK BITCH!");
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.CREEPER);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.CREEPER);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.CREEPER);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.CREEPER);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.CREEPER);
+			} else {
+				p.sendMessage(Main.NoTag + "Bunnies? §lBUNNIES!");
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.RABBIT);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.RABBIT);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.RABBIT);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.RABBIT);
+				p.getWorld().spawnCreature(p.getLocation(), CreatureType.RABBIT);
+			}
 		}
 		return true;
 	}
