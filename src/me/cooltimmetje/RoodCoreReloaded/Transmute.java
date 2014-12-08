a2982857e658f6b7eb7f623eb50d2ad3c12b8613
cooltimmetje <cooltimmetje@me.com>
2014-12-08 19:48:38 +0100

    added code transmute
    Signed-off-by: cooltimmetje <cooltimmetje@me.com>

--- src/me/cooltimmetje/RoodCoreReloaded/Transmute.java
+++ src/me/cooltimmetje/RoodCoreReloaded/Transmute.java
@@ -1,11 +1,14 @@
 package me.cooltimmetje.RoodCoreReloaded;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class Transmute implements Listener, CommandExecutor {
 
@@ -15,8 +18,8 @@ public class Transmute implements Listener, CommandExecutor {
 		if(cmd.getLabel().equalsIgnoreCase("transmute")){
 			Material transItem = p.getInventory().getItemInHand().getType();
 			if(!transItem.equals(null)){
-				if(transItem.equals(Material.LOG) || transItem.equals(Material.LOG)){
-					p.sendMessage("§7§oOpen inventory...");
+				if(transItem.equals(Material.LOG) || transItem.equals(Material.LOG_2)){
+					openLog(p);
 					return true;
 				} else {
 					p.sendMessage(Main.TransTag + Main.Error + "This item cannot be transmuted.");
@@ -31,4 +34,25 @@ public class Transmute implements Listener, CommandExecutor {
 		return false;
 	}
 
+	public void openLog(Player p){
+		ItemStack pItemHand = p.getInventory().getItemInHand();
+		Inventory log = Bukkit.createInventory(null, 9, "§9Transmute §8» §9Log");
+
+		ItemStack oakLog = new ItemStack(Material.LOG, 1, (byte)0);
+		log.setItem(0, oakLog);
+		ItemStack spruceLog = new ItemStack(Material.LOG, 1, (byte)1);
+		log.setItem(1, spruceLog);
+		ItemStack birchLog = new ItemStack(Material.LOG, 1, (byte)2);
+		log.setItem(2, birchLog);
+		ItemStack jungleLog = new ItemStack(Material.LOG, 1, (byte)3);
+		log.setItem(3, jungleLog);
+		ItemStack acaciaLog = new ItemStack(Material.LOG_2, 1, (byte)0);
+		log.setItem(4, acaciaLog);
+		ItemStack darkOakLog = new ItemStack(Material.LOG_2, 1, (byte)1);
+		log.setItem(5, darkOakLog);
+		
+		ItemStack transItem = new ItemStack(pItemHand.getType(), pItemHand.getAmount(), (short)pItemHand.getDurability());
+		log.setItem(8, transItem);	
+	}
+	
 }
