805f72872e19c3011e2c586e5c89d3c20d929dc7
cooltimmetje <cooltimmetje@me.com>
2014-12-08 21:20:13 +0100

    'Fixed' full inv bug
    Signed-off-by: cooltimmetje <cooltimmetje@me.com>

--- src/me/cooltimmetje/RoodCoreReloaded/Transmute.java
+++ src/me/cooltimmetje/RoodCoreReloaded/Transmute.java
@@ -13,25 +13,30 @@ public class Transmute implements Listener, CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player p = (Player) sender;
 		if(cmd.getLabel().equalsIgnoreCase("transmute")){
-			Material transItem = p.getInventory().getItemInHand().getType();
-			if(!transItem.equals(null)){
-				if(transItem.equals(Material.LOG) || transItem.equals(Material.LOG_2)){
-					TransmuteLog.openLog(p);
-					return true;
-				} else if(transItem.equals(Material.GRASS) || transItem.equals(Material.DIRT) || transItem.equals(Material.SAND)){
-					TransmuteDirt.openDirt(p);
-					return true;
+			if(!(p.getInventory().firstEmpty() == -1)){
+				Material transItem = p.getInventory().getItemInHand().getType();
+				if(!transItem.equals(null)){
+					if(transItem.equals(Material.LOG) || transItem.equals(Material.LOG_2)){
+						TransmuteLog.openLog(p);
+						return true;
+					} else if(transItem.equals(Material.GRASS) || transItem.equals(Material.DIRT) || transItem.equals(Material.SAND)){
+						TransmuteDirt.openDirt(p);
+						return true;
+					} else {
+						p.sendMessage(Main.TransTag + Main.Error + "This item cannot be transmuted.");
+						p.sendMessage(Main.NoTag + "Supported items: §7§oList coming SoonTM");
+						return false;
+					}
 				} else {
-					p.sendMessage(Main.TransTag + Main.Error + "This item cannot be transmuted.");
-					p.sendMessage(Main.NoTag + "Supported items: §7§oList coming SoonTM");
+					p.sendMessage(Main.TransTag + Main.Error + "You need to hold a item in your hand to transmutate.");
 					return false;
 				}
 			} else {
-				p.sendMessage(Main.TransTag + Main.Error + "You need to hold a item in your hand to transmutate.");
+				p.sendMessage(Main.TransTag + Main.Error + "Your inventory is full! You can't transmute with a full inventory. Clear out atleast one slot!");
 				return false;
 			}
 		}
 		return false;
 	}
-	
+
 }
