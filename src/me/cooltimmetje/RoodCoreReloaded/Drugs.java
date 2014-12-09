1cd6b93649dbe8d872e30590f37a83651d8ff9f7
cooltimmetje <cooltimmetje@me.com>
2014-12-09 19:27:31 +0100

    Fixed some mistakes
    Signed-off-by: cooltimmetje <cooltimmetje@me.com>

--- src/me/cooltimmetje/RoodCoreReloaded/Drugs.java
+++ src/me/cooltimmetje/RoodCoreReloaded/Drugs.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -14,21 +15,27 @@ public class Drugs implements Listener {
 	@EventHandler
 	public void onItemInteract(PlayerInteractEvent event){
 		Player p = event.getPlayer();
-		if(event.getItem().getType() == Material.ARROW && event.getAction() == Action.RIGHT_CLICK_AIR){
-			if(event.getItem().hasItemMeta() == true){
-				if(!p.hasPotionEffect(PotionEffectType.SPEED) || !p.hasPotionEffect(PotionEffectType.CONFUSION)){
-					p.sendMessage(Main.NoTag + "You feel a bit confused, but also very powerfull.");
-					p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2400, 2));
-					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 600, 0));
-					p.getInventory().removeItem(CustomRecipes.heroine());
-					return;
+		if(p.getItemInHand().getType() == Material.ARROW){
+			if(event.getAction() == Action.RIGHT_CLICK_AIR){
+				if(p.getItemInHand().hasItemMeta() == true){
+					if(!p.hasPotionEffect(PotionEffectType.SPEED) || !p.hasPotionEffect(PotionEffectType.CONFUSION)){
+						p.sendMessage(Main.NoTag + "You feel a bit confused, but also very powerfull.");
+						p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2400, 2));
+						p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 600, 0));
+						p.getInventory().removeItem(new ItemStack(CustomRecipes.heroine()));
+						return;
+					} else {
+						p.sendMessage(Main.NoTag + "§bYou think by yourself: §a§oI'd better not take another shot, I'm sure it will harm me.");
+						return;
+					}
 				} else {
-					p.sendMessage(Main.NoTag + "§bYou think by yourself: §a§oI'd better not take another shot, I'm sure it will harm me.");
 					return;
 				}
 			} else {
 				return;
 			}
+		} else {
+			return;
 		}
 	}
 
