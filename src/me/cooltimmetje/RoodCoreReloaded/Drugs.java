0159c8ccdd4bd4ad066b77033c232ea2bcfe1521
cooltimmetje <cooltimmetje@me.com>
2014-12-09 19:19:06 +0100

    Added eventhandler Added removeitem
    Signed-off-by: cooltimmetje <cooltimmetje@me.com>

--- src/me/cooltimmetje/RoodCoreReloaded/Drugs.java
+++ src/me/cooltimmetje/RoodCoreReloaded/Drugs.java
@@ -2,6 +2,7 @@ package me.cooltimmetje.RoodCoreReloaded;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -10,6 +11,7 @@ import org.bukkit.potion.PotionEffectType;
 
 public class Drugs implements Listener {
 
+	@EventHandler
 	public void onItemInteract(PlayerInteractEvent event){
 		Player p = event.getPlayer();
 		if(event.getItem().getType() == Material.ARROW && event.getAction() == Action.RIGHT_CLICK_AIR){
@@ -18,6 +20,7 @@ public class Drugs implements Listener {
 					p.sendMessage(Main.NoTag + "You feel a bit confused, but also very powerfull.");
 					p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2400, 2));
 					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 600, 0));
+					p.getInventory().removeItem(CustomRecipes.heroine());
 					return;
 				} else {
 					p.sendMessage(Main.NoTag + "§bYou think by yourself: §a§oI'd better not take another shot, I'm sure it will harm me.");
