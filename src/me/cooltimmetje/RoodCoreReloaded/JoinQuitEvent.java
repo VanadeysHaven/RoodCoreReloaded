4dd53d7cf37975a6087a06496a8180f00ccf48fa
cooltimmetje <cooltimmetje@me.com>
2014-12-16 20:50:31 +0100

    Added more join stuffs...

--- src/me/cooltimmetje/RoodCoreReloaded/JoinQuitEvent.java
+++ src/me/cooltimmetje/RoodCoreReloaded/JoinQuitEvent.java
@@ -1,7 +1,9 @@
 package me.cooltimmetje.RoodCoreReloaded;
 
+import io.puharesource.mc.titlemanager.api.ActionbarTitleObject;
 import io.puharesource.mc.titlemanager.api.TitleObject;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,10 +11,18 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinQuitEvent implements Listener {
 
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerJoin (PlayerJoinEvent event){
 		Player p = event.getPlayer();
-		new TitleObject("§6§lWelcome, " + p.getDisplayName(), TitleObject.TitleType.TITLE).setFadeIn(20).setStay(60).setFadeOut(20).send(p);
+		new TitleObject("§6§lWelcome, §9" + p.getName(), TitleObject.TitleType.TITLE).setFadeIn(20).setStay(60).setFadeOut(20).send(p);
+		new TitleObject("§6§lTo §c§lThe §4#§lTeamR00D §c§lNetwork", TitleObject.TitleType.SUBTITLE).setFadeIn(20).setStay(80).setFadeOut(20).send(p);
+		
+		for (Player pl : Bukkit.getOnlinePlayers()){
+			new ActionbarTitleObject(p.getName() + " §ajoined! §b- §8(§6" + Bukkit.getOnlinePlayers().length + "§8/§6" + Bukkit.getMaxPlayers() + "§8)").send(pl);
+		}
+		
+		event.setJoinMessage(Main.NoTag + p.getDisplayName() + " §ajoined! §b- §8(§6" + Bukkit.getOnlinePlayers().length + "§8/§6" + Bukkit.getMaxPlayers() + "§8)");
 	}
 
 }
